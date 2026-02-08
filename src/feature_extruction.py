4175c7e535fc541fa28eb84c34d0295e70fec87f
sanjaraT <sanjaratunola@gmail.com>
2026-02-08 21:23:16 +0600

    Feature Extraction

--- src/feature_extruction.py
+++ src/feature_extruction.py
@@ -6,3 +6,14 @@ def add_text_features(df):
     df['word_count'] = df['text'].apply(lambda x : len(x.split()))
     df['exclamation_count'] = df['text'].apply(lambda x : x.count('!'))
     return df
+
+def tfidif_features(train_text, test_text):
+    tfidf = TfidfVectorizer(
+        max_features=20000,
+        ngram_range=(1,2),
+        stop_words='english'
+    )
+    X_train = tfidf.fit_transform(train_text)
+    X_test = tfidf.transform(test_text)
+
+    return X_train, X_test, tfidf
