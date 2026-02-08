379e659b853ec132de4352aeb7c14abac078e2bc
sanjaraT <sanjaratunola@gmail.com>
2026-02-08 21:32:33 +0600

    Feature Extraction

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from src.eda import load_and_preview
 from src.preprocess import clean_text
+from src.feature_extruction import add_text_features, tfidif_features
+from sklearn.model_selection import train_test_split
 
 df = load_and_preview("data/Fake.csv", "data/True.csv")
 
 df['text'] = df['text'].apply(clean_text)
+
+df = add_text_features(df)
+X = df['text']
+y = df[['label']]
+
+#split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+#tfidf
+X_train_tfidf, X_test_tfidf, vectorizer = tfidif_features(X_train, X_test)
+
+
